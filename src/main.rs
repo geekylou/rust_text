ef2aae14eb948dcb9eb5e153a892a5974ad3f85f
geekylou <nn6215@googlemail.com>
2021-06-13 21:37:48 +0100

    Move decoder to a trait so we can switch between UTF-8 and Beeb mode on the fly in the future.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use
     std::io::Seek,
     std::fs::File,
     std::io::Read,
+    async_trait::async_trait,
 };
 
 
@@ -41,11 +42,12 @@ pub async fn main()
 
 pub async fn handle_connection(mut stream: tokio::net::TcpStream) -> Result<usize,tokio::io::Error>
 {   
+    let decoder = Mode7UTF8Ansi;
     println!("Connected.");
     let mut not_finished = true;
     stream.write_all(b"\x0c\x1E").await?; //Welcome To RustTex.\r\n").await?;
 
-    load_page_to_stream(&mut stream,"title.tti",-1).await?;
+    load_page_to_stream(&mut stream,"title.tti",-1,&decoder).await?;
     stream.write_all(b"\r\n").await?;
     while not_finished
     {
@@ -59,20 +61,20 @@ pub async fn handle_connection(mut stream: tokio::net::TcpStream) -> Result<usiz
         else if line == "help"
         {
             stream.write_all(b"\x0c\x1E").await?;
-            load_page_to_stream(&mut stream,"help.tti",-1).await?;
+            load_page_to_stream(&mut stream,"help.tti",-1,&decoder).await?;
         }
         else if line == "menu"
         {
             stream.write_all(b"\x0c\x1E").await?; //Welcome To RustTex.\r\n").await?;
 
-            load_page_to_stream(&mut stream,"title.tti",-1).await?;
+            load_page_to_stream(&mut stream,"title.tti",-1,&decoder).await?;
             stream.write_all(b"\r\n").await?;
         }
         else if line == "http"
         {
             stream.write_all(b"ADDR:").await?;
             let url = read_line(&mut stream).await?.to_lowercase();
-            load_page_from_addr(&mut stream, &url).await?;
+            load_page_from_addr(&mut stream, &url,&decoder).await?;
 
             stream.write_all(b"\r\n").await?;
         }
@@ -125,226 +127,250 @@ pub async fn read_line(stream: &mut tokio::net::TcpStream) -> Result<String,toki
 
 use pretty_hex::*;
 
-// This translates the escape codes to create text data which is understood by the BBC micro.
-// It will return Ok(1) if double height characters are used.
-pub async fn de_escape(stream: &mut tokio::net::TcpStream, buf:&[u8]) -> Result<u8,std::io::Error>
+struct Mode7BeebAscii;
+struct Mode7UTF8Ansi;
+
+#[async_trait]
+trait TTIDecoder 
 {
-    let mut repeat = 0;
+    // Static method signature; `Self` refers to the implementor type.
+    fn new() -> Self;
+    async fn de_escape(&self,stream: &mut tokio::net::TcpStream, buf:&[u8], line_b:bool) -> Result<u8,std::io::Error>;
+}
 
-    println!("{}", buf.hex_dump());
-
-    let mut prev = 0;
-    for i in buf
+#[async_trait]
+impl TTIDecoder for Mode7BeebAscii
+{
+    fn new() -> Mode7BeebAscii
     {
-        // Strip out carriage returns and line feeds as they shouldn't be within the line data 
-        // and we add our own.
-        if *i != b'\r' || *i != b'\n'
-        {
-            if prev == 0x1B
-            {
-                if *i == 0x4d
-                {
-                    repeat = 1;
-                }
-                println!("Esc:{}",*i-(0x40 as u8));
-                stream.write_u8(*i+(0x40 as u8)).await?;
-            }
-            else if *i != 0x1B 
-            {
-                let mut ch = *i;
-                if *i > 0x20 && *i < 0x80
-                {
-                    ch = *i + 128;
-                }
-                stream.write_u8(ch).await?;
-            }
-        prev = *i;
-        }
+        Mode7BeebAscii{}
     }
+    // This translates the escape codes to create text data which is understood by the BBC micro.
+    // It will return Ok(1) if double height characters are used.
+    async fn de_escape(&self,stream: &mut tokio::net::TcpStream, buf:&[u8],_line_b:bool) -> Result<u8,std::io::Error>
+    {
+        let mut repeat = 0;
 
-    return Ok(repeat);
+        println!("{}", buf.hex_dump());
+
+        let mut prev = 0;
+        for i in buf
+        {
+            // Strip out carriage returns and line feeds as they shouldn't be within the line data 
+            // and we add our own.
+            if *i != b'\r' || *i != b'\n'
+            {
+                if prev == 0x1B
+                {
+                    if *i == 0x4d
+                    {
+                        repeat = 1;
+                    }
+                    println!("Esc:{}",*i-(0x40 as u8));
+                    stream.write_u8(*i+(0x40 as u8)).await?;
+                }
+                else if *i != 0x1B 
+                {
+                    let mut ch = *i;
+                    if *i > 0x20 && *i < 0x80
+                    {
+                        ch = *i + 128;
+                    }
+                    stream.write_u8(ch).await?;
+                }
+            prev = *i;
+            }
+        }
+
+        return Ok(repeat);
+    }
 }
 
 
-
-
-// This translates the escape codes to create text data which is understood by the BBC micro.
-// It will return Ok(1) if double height characters are used.
-pub async fn de_escape_mode7_utf(stream: &mut tokio::net::TcpStream, buf:&[u8], line_b:bool) -> Result<u8,std::io::Error>
+#[async_trait]
+impl TTIDecoder for Mode7UTF8Ansi
 {
-    let mut repeat = 0;
-
-    println!("{}", buf.hex_dump());
-    let mut foreground_colour = 6;
-    let mut double_height = false;
-    let mut graphics = false;
-    let mut seperated = false;
-    let mut prev = 0;
-    for i in buf
+    fn new() -> Mode7UTF8Ansi
     {
-        // Strip out carriage returns and line feeds as they shouldn't be within the line data 
-        // and we add our own.
-        if *i != b'\r' || *i != b'\n'
+        Mode7UTF8Ansi{}
+    }
+    // This translates the escape codes to create text data which is understood by the BBC micro.
+    // It will return Ok(1) if double height characters are used.
+    async fn de_escape(&self,stream: &mut tokio::net::TcpStream, buf:&[u8], line_b:bool) -> Result<u8,std::io::Error>
+    {
+        let mut repeat = 0;
+
+        println!("{}", buf.hex_dump());
+        let mut foreground_colour = 6;
+        let mut double_height = false;
+        let mut graphics = false;
+        let mut seperated = false;
+        let mut prev = 0;
+        for i in buf
         {
-            let mut ch = None;
-            if prev == 0x1B
+            // Strip out carriage returns and line feeds as they shouldn't be within the line data 
+            // and we add our own.
+            if *i != b'\r' || *i != b'\n'
             {
-                /*if *i == 0x4d
+                let mut ch = None;
+                if prev == 0x1B
                 {
-                    repeat = 1;
-                }*/
-                println!("Esc:{}",*i-(0x40 as u8));
-
-                ch = Some(*i+(0x40 as u8));
-            }
-            else if *i != 0x1B 
-            {
-                ch = Some(*i);
-            }
-        
-        if let Some(mut ch) = ch 
-        {
-            print!("ch:{} ",ch);
-            if ch >= 145 && ch <= 151
-            {
-                graphics = true;
-                foreground_colour = ch - 145;
-                stream.write_all("\u{001b}[".as_bytes()).await?;
-                
-                let colour = String::from((ch-145+31).to_string()+";1m ");
-                stream.write_all(colour.as_bytes()).await?;
-            } 
-            else if ch >= 129 && ch <= 135
-            {   
-                graphics = false; 
-                foreground_colour = ch - 129;
-                stream.write_all("\u{001b}[".as_bytes()).await?;
-
-                let colour = String::from((ch-129+31).to_string()+";1m ");
-                stream.write_all(colour.as_bytes()).await?;
-            }
-            else if ch == 157
-            { 
-                stream.write_all("\u{001b}[".as_bytes()).await?;
-
-                let colour = String::from((foreground_colour+101).to_string()+";1m ");
-                stream.write_all(colour.as_bytes()).await?;
-
-            }
-            else if ch == 156
-            { 
-                stream.write_all("\u{001b}[40;1m".as_bytes()).await?;
-            }
-            else
-            {   
-                let mut base_code = 0xe000;
-
-                if seperated
-                {
-                    base_code = 0xe0c0;
-                }
-                if double_height
-                {
-                    if line_b
+                    /*if *i == 0x4d
                     {
-                        base_code = base_code + 0x80
+                        repeat = 1;
+                    }*/
+                    println!("Esc:{}",*i-(0x40 as u8));
+
+                    ch = Some(*i+(0x40 as u8));
+                }
+                else if *i != 0x1B 
+                {
+                    ch = Some(*i);
+                }
+            
+            if let Some(mut ch) = ch 
+            {
+                print!("ch:{} ",ch);
+                if ch >= 145 && ch <= 151
+                {
+                    graphics = true;
+                    foreground_colour = ch - 145;
+                    stream.write_all("\u{001b}[".as_bytes()).await?;
+                    
+                    let colour = String::from((ch-145+31).to_string()+";1m ");
+                    stream.write_all(colour.as_bytes()).await?;
+                } 
+                else if ch >= 129 && ch <= 135
+                {   
+                    graphics = false; 
+                    foreground_colour = ch - 129;
+                    stream.write_all("\u{001b}[".as_bytes()).await?;
+
+                    let colour = String::from((ch-129+31).to_string()+";1m ");
+                    stream.write_all(colour.as_bytes()).await?;
+                }
+                else if ch == 157
+                { 
+                    stream.write_all("\u{001b}[".as_bytes()).await?;
+
+                    let colour = String::from((foreground_colour+101).to_string()+";1m ");
+                    stream.write_all(colour.as_bytes()).await?;
+
+                }
+                else if ch == 156
+                { 
+                    stream.write_all("\u{001b}[40;1m".as_bytes()).await?;
+                }
+                else
+                {   
+                    let mut base_code = 0xe000;
+
+                    if seperated
+                    {
+                        base_code = 0xe0c0;
+                    }
+                    if double_height
+                    {
+                        if line_b
+                        {
+                            base_code = base_code + 0x80
+                        }
+                        else
+                        {
+                            base_code = base_code + 0x40
+                        }
+                    }
+
+                    if ch == 153
+                    {
+                        seperated = false;
+                        ch = b' ';
+                    }
+                    if ch == 154
+                    {
+                        seperated = true;
+                        ch = b' ';
+                    }
+                    if ch == 141
+                    {
+                        repeat = 1;
+                        double_height = true;
+                        ch = b' ';
+                    }
+                    else if ch == 140
+                    {
+                        double_height = false;
+                        ch = b' ';
+                    }
+                    if graphics && ch >= 95 && ch <= 127 
+                    {
+                        ch = ch + 128;
+                    }
+                    
+                    if graphics && ch > 160 && ch <= 191
+                    {
+                        let mut buf = [0;3];
+                        let x = char::from_u32(((ch-160) as u32) + base_code + 0x200);
+                        
+                        let x = x.unwrap();
+
+                        x.encode_utf8(&mut buf);
+
+                        stream.write(&buf).await?;
+                    }
+                    if graphics && ch >= 224 
+                    {
+                        let mut buf = [0;3];
+                        let x = char::from_u32(((ch-192) as u32) + base_code + 0x200);
+                        
+                        let x = x.unwrap();
+
+                        x.encode_utf8(&mut buf);
+
+                        stream.write(&buf).await?;
+                    }
+                    else if graphics==true && ch > 32 && ch <= 63 //&& ch != 35
+                    {
+                        let mut buf = [0;3];
+                        let x = char::from_u32((ch as u32) + base_code + 0x200 - 32);
+                        
+                        let x = x.unwrap();
+
+                        x.encode_utf8(&mut buf);
+
+                        stream.write(&buf).await?;
+                    }
+                    else if double_height
+                    {
+                        let mut shift = 0xe000;
+                        if line_b
+                        {
+                            shift = 0xe100;
+                        }
+                        let mut buf = [0;3];
+                        let x = char::from_u32((ch as u32) + shift);
+                        let x = x.unwrap();
+
+                        x.encode_utf8(&mut buf);
+
+                        stream.write(&buf).await?;
                     }
                     else
                     {
-                        base_code = base_code + 0x40
+                        stream.write_u8(ch).await?;
                     }
-                }
-
-                if ch == 153
-                {
-                    seperated = false;
-                    ch = b' ';
-                }
-                if ch == 154
-                {
-                    seperated = true;
-                    ch = b' ';
-                }
-                if ch == 141
-                {
-                    repeat = 1;
-                    double_height = true;
-                    ch = b' ';
-                }
-                else if ch == 140
-                {
-                    double_height = false;
-                    ch = b' ';
-                }
-                if graphics && ch >= 95 && ch <= 127 
-                {
-                    ch = ch + 128;
-                }
-                
-                if graphics && ch > 160 && ch <= 191
-                {
-                    let mut buf = [0;3];
-                    let x = char::from_u32(((ch-160) as u32) + base_code + 0x200);
-                    
-                    let x = x.unwrap();
-
-                    x.encode_utf8(&mut buf);
-
-                    stream.write(&buf).await?;
-                }
-                if graphics && ch >= 224 
-                {
-                    let mut buf = [0;3];
-                    let x = char::from_u32(((ch-192) as u32) + base_code + 0x200);
-                    
-                    let x = x.unwrap();
-
-                    x.encode_utf8(&mut buf);
-
-                    stream.write(&buf).await?;
-                }
-                else if graphics==true && ch > 32 && ch <= 63 //&& ch != 35
-                {
-                    let mut buf = [0;3];
-                    let x = char::from_u32((ch as u32) + base_code + 0x200 - 32);
-                    
-                    let x = x.unwrap();
-
-                    x.encode_utf8(&mut buf);
-
-                    stream.write(&buf).await?;
-                }
-                else if double_height
-                {
-                    let mut shift = 0xe000;
-                    if line_b
-                    {
-                        shift = 0xe100;
-                    }
-                    let mut buf = [0;3];
-                    let x = char::from_u32((ch as u32) + shift);
-                    let x = x.unwrap();
-
-                    x.encode_utf8(&mut buf);
-
-                    stream.write(&buf).await?;
-                }
-                else
-                {
-                    stream.write_u8(ch).await?;
                 }
             }
+            prev = *i;
+            }
         }
-        prev = *i;
-        }
+        stream.write_all("\u{001b}[37;1m\u{001b}[40m".as_bytes()).await?;
+        stream.write_all(b"\r\n").await?;
+        return Ok(repeat);
     }
-    stream.write_all("\u{001b}[37;1m\u{001b}[40m".as_bytes()).await?;
-    stream.write_all(b"\r\n").await?;
-    return Ok(repeat);
 }
 
-
-pub async fn load_page_from_addr(stream_out: &mut tokio::net::TcpStream,url_str: &str) -> Result<u8,std::io::Error>
+async fn load_page_from_addr(stream_out: &mut tokio::net::TcpStream,url_str: &str,decoder: &impl TTIDecoder) -> Result<u8,std::io::Error>
 {
     use hyper::Client;
     let client = Client::new();
@@ -365,7 +391,7 @@ pub async fn load_page_from_addr(stream_out: &mut tokio::net::TcpStream,url_str:
                 if let Ok(buf) = buf_r
                 {
                     stream_out.write_all(b"\x0c").await?;
-                    render_page_to_stream(stream_out,&buf, -1).await?;
+                    render_page_to_stream(stream_out,&buf, -1,decoder).await?;
                 }
                 
             }
@@ -378,7 +404,7 @@ pub async fn load_page_from_addr(stream_out: &mut tokio::net::TcpStream,url_str:
 
 
 
-pub async fn load_page_to_stream(stream: &mut tokio::net::TcpStream,filename: &str, page_no:i32) -> Result<i32,std::io::Error>
+async fn load_page_to_stream(stream: &mut tokio::net::TcpStream,filename: &str, page_no:i32,decoder: &impl TTIDecoder) -> Result<i32,std::io::Error>
 {
    
     let mut buf = Vec::new();
@@ -390,7 +416,7 @@ pub async fn load_page_to_stream(stream: &mut tokio::net::TcpStream,filename: &s
     println!("Buf:{}",x);
     //std::io::Write::write_all(&mut std::io::stdout(),&buf);
 
-    return render_page_to_stream(stream,&buf, page_no).await
+    return render_page_to_stream(stream,&buf, page_no,decoder).await
 }
 
 // Renders the page TTI file stored in buf to stream.  
@@ -398,7 +424,7 @@ pub async fn load_page_to_stream(stream: &mut tokio::net::TcpStream,filename: &s
 // returns 
 // Ok(page_no) = Page no of the page found.
 // 
-pub async fn render_page_to_stream(stream: &mut tokio::net::TcpStream,buf: &[u8], requested_page_no:i32) -> Result<i32,std::io::Error>
+async fn render_page_to_stream(stream: &mut tokio::net::TcpStream,buf: &[u8], requested_page_no:i32,decoder: &impl TTIDecoder) -> Result<i32,std::io::Error>
 {
     let mut page_no = -1; // When this is -1 it indicates to the render engine that no page has been found.
 
@@ -499,9 +525,9 @@ pub async fn render_page_to_stream(stream: &mut tokio::net::TcpStream,buf: &[u8]
                 {
                     stream.write(b"\r\n").await?;
                 }
-                if de_escape_mode7_utf(stream,&buf[y..x-1],false).await? == 1
+                if decoder.de_escape(stream,&buf[y..x-1],false).await? == 1
                 {
-                    de_escape_mode7_utf(stream,&buf[y..x-1],true).await?;
+                    decoder.de_escape(stream,&buf[y..x-1],true).await?;
                     cur_ol = cur_ol + 1;
                 }
                 //stream.write(&buf[y..x-1]).await?;
